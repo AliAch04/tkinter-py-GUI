43148424d281fb71394e6a1163dcbe4f23baa41b
Ali ACHENAN <ali.jallousy12@gmail.com>
2025-09-28 22:16:41 +0100

    buttons

--- main.py
+++ main.py
@@ -28,8 +28,25 @@ label = Label(window,
               padx=30,
               pady=5,
               image=label_photo,
-              compound='bottom')
+              compound='bottom',
+              relief='solid',
+              borderwidth=2)
 label.pack() #activate the label
 #label.place(x=0, y=50)
+
+#--------Button--------#
+#create and config the button
+click_cnt = 0
+def click():
+    global click_cnt
+    print(f'you clicked : {click_cnt}')
+    click_cnt +=1
+
+button = Button(window,
+                text='Click',
+                command=click,
+                font=('Comic Sans', 30)) 
+button.pack(pady=10) # Added padding to separate from label
+
 
 window.mainloop() #activate the window and listen for events!
