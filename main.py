aff5c0a4f1a04f0f530cfa28c744e95a1d76e882
Ali ACHENAN <ali.jallousy12@gmail.com>
2025-09-28 19:06:20 +0100

    added window

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from tkinter import *
+from PIL import Image, ImageTk
 
+#--------Window--------#
 window = Tk() #instantiate
+window.geometry("580x580")
+window.title('1st tkinter GUI')
 
-window.mainloop()
+# convert image to PhotoImage to use it as icon for the window
+img = Image.open('zoz_logo.jpg')
+# img.show()
+icon = ImageTk.PhotoImage(img)
+window.iconphoto(True, icon)
+
+#changing the window
+window.config(background="#2a3cc6")
+
+
+
+window.mainloop() #activate the window and listen for events!
