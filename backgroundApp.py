ffba122e0e7e3517182a208fd04bbcab411b82b5
Ali ACHENAN <ali.jallousy12@gmail.com>
2025-09-29 17:56:22 +0100

    Added error logging and manual testing and stop window functionality

--- backgroundApp.py
+++ backgroundApp.py
@@ -20,7 +20,7 @@ class BackgroundApp:
     def setup_gui(self):
         """Configure l'interface graphique"""
         self.root.title("Application Background + Tkinter")
-        self.root.geometry("400x300")
+        self.root.geometry("600x500")
         
         # Frame principal
         main_frame = tk.Frame(self.root, 
@@ -74,14 +74,82 @@ class BackgroundApp:
         
         tk.Button(button_frame, text="Arrêter", command=self.stop_app,
                  bg="lightcoral").pack(side=tk.LEFT, padx=5)
+        
+    def log_message(self, msg):
+        '''Ajouter un message au journal (log_text)'''
+        time_stamp = datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
+        entry_log = self.log_text.insert(tk.END, f' {time_stamp} , {msg} ')
+
+    def show_error_message(self):
+        '''Afficher un erreur aléatoire dans boite de dialog'''
+        errors = ['erreur1', 'erreur2', 'erreur3', 'erreur4', 'erreur5', 'erreur6']
+
+        err_choise = random.choice(errors)
+        self.error_count += 1
+
+        # Afficher le msg en boite dialog
+        messagebox.showerror('Erreur Système', err_choise)
+
+        # MAJ et afficher le conteur
+        self.error_label.config(text=f"Erreurs affichées: {self.error_count}")
+
+        # Affirmer dans le journal
+        self.log_text.insert(tk.END, f'Erreur : "{err_choise}" ')
+        self.log_text.see(tk.END)
+
+
+        
+    def background_worker_1(self):
+        """1er background qui génére des erreurs chaque 5 seconds"""
+        self.log_message('Thread d\'erreur démarré')
+
+        while self.running == True:
+            # ajouter try/except 
+            self.show_error_message()
+
+            for i in range(50):
+                if not self.running:
+                    break
+                time.sleep(0.1)
+
+    def background_worker_2(self):
+        """2er background qui génére des erreurs chaque 10 seconds"""
+        self.log_message('Thread d\'erreur démarré')
+
+        while self.running == True:
+            # ajouter try/except 
+            self.show_error_message()
+
+            for i in range(100):
+                if not self.running:
+                    break
+                time.sleep(0.1)
     
     def start_threads(self):
-        pass
+        """Démarrer la totalités des threads"""
+        # Creation des threads
+        self.background_thread_1 = threading.Thread(target=self.background_worker_1, daemon=True)
+        self.background_thread_2 = threading.Thread(target=self.background_worker_2, daemon=True)
+
+        # Demarrage des threads
+        self.background_thread_1.start()
+        self.background_thread_2.start()
+
+        self.log_message('Tous les threads demarrés')
+
+
     
     def manual_test(self):
-        print('Teste manualle activé! ')
+        """Tester manuellement des threats (erreurs)"""
+        self.show_error_message()
+        self.log_message(" -- Test manuel déclenché!")
     
     def stop_app(self):
+        """Arreter l'app proprement"""
+        self.running = False
+        self.status_label.config(text='Application en cours de fermeture...', fg='red')
+
+        self.root.after(1500, self.root.destroy)
         print('Application a été arretée proprement!')
 
 def main():
